98ae7a168c4d0069ff3b7ec6a491fdfe98bbe79a
Xavi Lee <awxiaoxian2020@163.com>
2024-01-06 23:50:54 +0800

    update

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ turndownService.addRule("pre", {
 
 turndownService.keep('span');
 const filePath = path.join(__dirname, "getting-started.html"); // Replace 'your-file.html' with the actual file name
+const fileContent = fs.readFileSync(filePath, 'utf-8');
+const mainRegex = /<main\b[^>]*>([\s\S]*?)<\/main>/i;
+const match = fileContent.match(mainRegex);
+const mainContent = match ? match[0] : '';
+fs.writeFileSync(filePath, mainContent, 'utf-8');
 const markdown = turndownService.turndown(fs.readFileSync(filePath, "utf8"));
-
 fs.writeFileSync("output.md", markdown); // Replace 'output.md' with the desired output file name
 console.log("Markdown file created successfully!");
